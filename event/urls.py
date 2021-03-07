ba3dd80897d1d710e6d09b9abfc613a6dbad41a0
Avi Gupta <avibilasgupta@gmail.com>
2021-03-07 12:13:14 +0530

    final update

--- event/urls.py
+++ event/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "event"
 urlpatterns = [
-    path('event/', views.EventView.index, name='home'),
+    path('', views.EventView.index, name='home'),
     path('create/', views.add_event, name="createEvent"),
     path('liked/', views.likedEvents, name="likedEvents"),
     path('like/', views.like, name="like")
